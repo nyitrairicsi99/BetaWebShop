eaf057740b6cbb9eeaf9f2480504d21e44188faa
nyitrairicsi99 <58612330+nyitrairicsi99@users.noreply.github.com>
2022-01-11 16:26:44 +0100

    date autofill classes

--- MVC/View/themes/default/src/js/index.js
+++ MVC/View/themes/default/src/js/index.js
@@ -1,1 +1,23 @@
-console.log("JS working");
+Date.prototype.toDateInputValue = (function() {
+    var local = new Date(this);
+    local.setMinutes(this.getMinutes() - this.getTimezoneOffset());
+    return local.toJSON().slice(0,10);
+});
+
+Date.prototype.addHours = function(h) {
+    this.setTime(this.getTime() + (h*60*60*1000));
+    return this;
+  }
+
+window.onload = (event) => {
+    let dateToday = document.getElementsByClassName("autofill-today");
+    for (let i = 0; i < dateToday.length; i++) {
+        const element = dateToday[i];
+        element.value = new Date().toDateInputValue();
+    }
+    let dateHuge = document.getElementsByClassName("autofill-huge");
+    for (let i = 0; i < dateHuge.length; i++) {
+        const element = dateHuge[i];
+        element.value = (new Date()).addHours(24*365.25*12).toDateInputValue();
+    }
+};
